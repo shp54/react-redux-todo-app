82057b7329c167385ea1c0e8d50faa0593f0faa3
Shameek Poddar <spoddar@leveldata.com>
2016-05-29 11:15:42 -0400

    Split reducer

--- reducers/todos.js
+++ reducers/todos.js
@@ -1,11 +1,21 @@
 import Immutable from 'immutable'
 
+const todos = (todos, action) => {
+	switch(action.type){
+	case 'addTodo':
+		return todos.push(Immutable.Map({text: action.text, completed: false}))
+	case 'deleteTodo':
+		return todos.setIn([action.index, 'completed'], true)
+	default:
+		return todos
+	}
+}
+
 export default (state, action) => {
 	switch(action.type){
 		case 'addTodo':
-			return state.set('todos', state.get('todos').push(Immutable.Map({text: action.text, completed: false})))
 		case 'deleteTodo':
-			return state.set('todos', state.get('todos').setIn([action.index, 'completed'], true))
+			return state.set('todos', todos(state.get('todos'), action))
 		default:
 			return state
 	}
